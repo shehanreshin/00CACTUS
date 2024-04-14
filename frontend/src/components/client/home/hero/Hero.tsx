99d847e7b1f20858f2b49d7ac80bc528c1919d7a
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-14 20:42:43 +0530

    Make text in hero section responsive

--- frontend/src/components/client/home/hero/Hero.tsx
+++ frontend/src/components/client/home/hero/Hero.tsx
@@ -2,12 +2,10 @@ import fonts from "@/utils/Fonts";
 
 export default function Hero() {
     return (
-        <section className={`flex flex-col ${fonts.vanchrome.className} text-[15.8vw] leading-[0.79] tracking-npx2 justify-center gap-5`}>
+        <section className={`flex flex-col ${fonts.vanchrome.className} 2xl:text-[15.8vw] xl:text-[17vw] lg:text-[17.2vw] md:text-[17.2vw] leading-[0.79] tracking-npx2 justify-center gap-5`}>
             <div className={`flex gap-10`}>
                 <div><span className={`me-[9px]`}>EMBRACE</span>YOUR</div>
-                <div className={`flex`}>
-                    <div className={`w-full h-full bg-foreground rounded-full`}></div>
-                </div>
+                <div className={`w-48 bg-foreground rounded-full`}></div>
             </div>
             <div>TRUEST<span className={`me-[9px] ms-[9px]`}>SELF</span>WITH</div>
             <div className={`flex gap-10`}>
