b8f3fe7a16d8bdc5b5b22d92b44499f145885d2d
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-14 15:35:26 +0530

    Create basic skeleton of navbar

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        gallery: "var(--gallery)",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "var(--ring)",
