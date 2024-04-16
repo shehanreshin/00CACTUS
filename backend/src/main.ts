33883b46ae0508d9943e084b6783efbf63f03562
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-16 18:08:39 +0530

    Update User related schemas to include date time stamps for logging purposes and define schema for Category

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,8 +13,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
-  await app.listen(5000);
+  await app.listen(process.env.SERVER_PORT || 5000);
 }
 bootstrap();
