1ee0aec67bf20c9f3c8da82648444f8bb020fdc8
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-22 11:31:43 +0530

    Add functionality to create a country

--- backend/src/modules/countries/countries-prisma.service.ts
+++ backend/src/modules/countries/countries-prisma.service.ts
@@ -1,10 +1,15 @@
 import { CountriesService } from './countries.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { CreateCountryDto } from './dto/create-country.dto';
 
 @Injectable()
 export class CountriesPrismaService implements CountriesService {
   constructor(private readonly prisma: PrismaService) {}
+
+  createCountry(countryDto: CreateCountryDto) {
+    return this.prisma.country.create({ data: countryDto });
+  }
   findAllCountries() {
     return this.prisma.country.findMany();
   }
--- backend/src/modules/countries/countries.service.ts
+++ backend/src/modules/countries/countries.service.ts
@@ -1,5 +1,8 @@
+import { CreateCountryDto } from './dto/create-country.dto';
+
 export abstract class CountriesService {
   abstract findAllCountries();
   abstract findCountry(id: string);
   abstract findCountryByName(name: string);
+  abstract createCountry(countryDto: CreateCountryDto);
 }
