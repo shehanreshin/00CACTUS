32cf5a208865ab05884c33d9c8ac48b527458d3e
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-23 15:49:25 +0530

    Add exception handling in addresses prisma service and return types in service layer of addresses for type safety

--- backend/src/modules/addresses/addresses-prisma.service.ts
+++ backend/src/modules/addresses/addresses-prisma.service.ts
@@ -4,24 +4,32 @@ import { PrismaService } from '../prisma/prisma.service';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { plainToInstance } from 'class-transformer';
 import { AddressResponseDto } from './dto/address-response.dto';
+import { CreationFailedException } from '../../common/exceptions/creation-failed.exception';
+import { ResourceNotFoundException } from '../../common/exceptions/resource-not-found.exception';
 
 @Injectable()
 export class AddressesPrismaService implements AddressesService {
   constructor(private readonly prisma: PrismaService) {}
-  findAddress(id: string) {
-    return this.prisma.address.findUnique({
+  async findAddress(id: string): Promise<AddressResponseDto> {
+    const address = await this.prisma.address.findUnique({
       where: { id },
       include: { country: true },
     });
+    if (!address) throw new ResourceNotFoundException('Address not found');
+
+    return plainToInstance(AddressResponseDto, address);
   }
 
-  async createAddress(addressDto: CreateAddressDto) {
+  async createAddress(
+    addressDto: CreateAddressDto,
+  ): Promise<AddressResponseDto> {
     const address = await this.prisma.address.create({
       data: addressDto,
       include: {
         country: true,
       },
     });
+    if (!address.id) throw new CreationFailedException('Address not created');
 
     return plainToInstance(AddressResponseDto, address);
   }
