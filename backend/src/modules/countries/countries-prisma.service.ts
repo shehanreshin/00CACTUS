f31e0d798c17afff42478de5e3b86fd07a06d896
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-23 15:43:32 +0530

    Add exception handling in countries prisma service and return types in service layer of countries for type safety

--- backend/src/modules/countries/countries-prisma.service.ts
+++ backend/src/modules/countries/countries-prisma.service.ts
@@ -2,26 +2,39 @@ import { CountriesService } from './countries.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateCountryDto } from './dto/create-country.dto';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 import { CountryResponseDto } from './dto/country-response.dto';
+import { CreationFailedException } from '../../common/exceptions/creation-failed.exception';
+import { ResourceNotFoundException } from '../../common/exceptions/resource-not-found.exception';
 
 @Injectable()
 export class CountriesPrismaService implements CountriesService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createCountry(countryDto: CreateCountryDto) {
-    return plainToClass(
+  async createCountry(
+    countryDto: CreateCountryDto,
+  ): Promise<CountryResponseDto> {
+    const country = await this.prisma.country.create({ data: countryDto });
+    if (!country.id) throw new CreationFailedException('Country not created');
+
+    return plainToInstance(CountryResponseDto, country);
+  }
+  async findAllCountries(): Promise<CountryResponseDto[]> {
+    return plainToInstance(
       CountryResponseDto,
-      await this.prisma.country.create({ data: countryDto }),
+      await this.prisma.country.findMany(),
     );
   }
-  findAllCountries() {
-    return this.prisma.country.findMany();
+  async findCountry(id: string): Promise<CountryResponseDto> {
+    const country = await this.prisma.country.findUnique({ where: { id } });
+    if (!country) throw new ResourceNotFoundException('Country not found');
+
+    return plainToInstance(CountryResponseDto, country);
   }
-  findCountry(id: string) {
-    return this.prisma.country.findUnique({ where: { id } });
-  }
-  findCountryByName(name: string) {
-    return this.prisma.country.findUnique({ where: { name } });
+  async findCountryByName(name: string): Promise<CountryResponseDto> {
+    const country = await this.prisma.country.findUnique({ where: { name } });
+    if (!country) throw new ResourceNotFoundException('Country not found');
+
+    return plainToInstance(CountryResponseDto, country);
   }
 }
--- backend/src/modules/countries/countries.service.ts
+++ backend/src/modules/countries/countries.service.ts
@@ -1,8 +1,11 @@
 import { CreateCountryDto } from './dto/create-country.dto';
+import { CountryResponseDto } from './dto/country-response.dto';
 
 export abstract class CountriesService {
-  abstract findAllCountries();
-  abstract findCountry(id: string);
-  abstract findCountryByName(name: string);
-  abstract createCountry(countryDto: CreateCountryDto);
+  abstract findAllCountries(): Promise<CountryResponseDto[]>;
+  abstract findCountry(id: string): Promise<CountryResponseDto>;
+  abstract findCountryByName(name: string): Promise<CountryResponseDto>;
+  abstract createCountry(
+    countryDto: CreateCountryDto,
+  ): Promise<CountryResponseDto>;
 }
