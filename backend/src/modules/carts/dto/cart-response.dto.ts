07490c6e2f82a5bf858b1b8c25634f306c2b185e
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-27 16:54:21 +0530

    Update types in cart response dto

--- backend/src/modules/carts/dto/cart-response.dto.ts
+++ backend/src/modules/carts/dto/cart-response.dto.ts
@@ -1,6 +1,8 @@
 import { IsUUID, ValidateNested } from 'class-validator';
-import { Expose } from 'class-transformer';
+import { Exclude, Expose, Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
+import { CartItemResponseDto } from '../../cart-item/dto/cart-item-response.dto';
+import { CustomerResponseDto } from '../../customers/dto/customer-response.dto';
 
 export class CartResponseDto {
   @IsUUID()
@@ -8,8 +10,18 @@ export class CartResponseDto {
   @ApiProperty({ example: '2c03fe0a-afe5-4b13-8bf1-6a7787017515' })
   id: string;
 
+  @Type(() => CustomerResponseDto)
   @ValidateNested()
+  @Exclude()
+  customer: CustomerResponseDto;
+
+  @IsUUID()
+  @Exclude()
+  customerId: string;
+
+  @ValidateNested()
+  @Type(() => CartItemResponseDto)
   @Expose({ name: 'CartItem' })
-  @ApiProperty({ isArray: true })
-  items: string[];
+  @ApiProperty({ isArray: true, type: CartItemResponseDto })
+  items: CartItemResponseDto[];
 }
