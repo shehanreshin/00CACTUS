406205337caa3a640abea01592b9dc99b591d3bd
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-27 18:19:24 +0530

    Add implementation for service layer of carts with prisma

--- backend/src/modules/carts/carts-prisma.service.ts
+++ backend/src/modules/carts/carts-prisma.service.ts
@@ -3,18 +3,20 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreationFailedException } from '../../common/exceptions/creation-failed.exception';
 import { ResourceNotFoundException } from '../../common/exceptions/resource-not-found.exception';
+import { plainToInstance } from 'class-transformer';
+import { CartResponseDto } from './dto/cart-response.dto';
 
 @Injectable()
 export class CartsPrismaService implements CartsService {
   constructor(private readonly prisma: PrismaService) {}
   async createCart(customerId: string) {
     const cart = await this.prisma.cart.create({
-      data: { customerId },
+      data: { customer: { connect: { id: customerId } } },
       include: { CartItem: true },
     });
     if (!cart) throw new CreationFailedException('Cart not created');
 
-    return cart;
+    return plainToInstance(CartResponseDto, cart);
   }
   async findCartByCustomerId(customerId: string) {
     const cart = await this.prisma.cart.findUnique({
@@ -23,6 +25,6 @@ export class CartsPrismaService implements CartsService {
     });
     if (!cart) throw new ResourceNotFoundException('Cart not found');
 
-    return cart;
+    return plainToInstance(CartResponseDto, cart);
   }
 }
