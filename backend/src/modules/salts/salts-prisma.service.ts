5943ba1e33c846fe1abdc0e14c08c210ece09578
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-28 20:15:09 +0530

    Add exception handling to find salt by user id method in salt prisma service

--- backend/src/modules/salts/salts-prisma.service.ts
+++ backend/src/modules/salts/salts-prisma.service.ts
@@ -5,16 +5,18 @@ import { Injectable } from '@nestjs/common';
 import { CreationFailedException } from '../../common/exceptions/creation-failed.exception';
 import { plainToInstance } from 'class-transformer';
 import { SaltResponseDto } from './dto/salt-response.dto';
+import { ResourceNotFoundException } from '../../common/exceptions/resource-not-found.exception';
 
 @Injectable()
 export class SaltsPrismaService implements SaltsService {
   constructor(private readonly prisma: PrismaService) {}
   async findSaltByUserId(userId: string): Promise<string> {
-    return (
-      await this.prisma.salt.findUnique({
-        where: { userId },
-      })
-    ).salt;
+    const salt = await this.prisma.salt.findUnique({
+      where: { userId },
+    });
+    if (!salt) throw new ResourceNotFoundException('Salt not found');
+
+    return salt.salt;
   }
   async createSalt(createSaltDto: CreateSaltDto): Promise<SaltResponseDto> {
     const salt = await this.prisma.salt.create({ data: createSaltDto });
