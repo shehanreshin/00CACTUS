1f8534400d9431c57b6e4f99ff6e3ca873d0ee7f
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-28 21:09:06 +0530

    Create an endpoint for login

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { LoginCredentialsDto } from './dto/login-credentials.dto';
+import { AuthService } from './auth.service';
 
 @ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() credentials: LoginCredentialsDto) {
+    return this.authService.login(credentials);
+  }
+}
