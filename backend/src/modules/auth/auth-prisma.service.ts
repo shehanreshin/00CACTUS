e08a41b8f9ceda69546ba3965a68e59d0ae3146e
shehanreshin <asp.shehanreshin@gmail.com>
2024-04-28 20:13:00 +0530

    Implement a method to validate login credentials in auth prisma service

--- backend/src/modules/auth/auth-prisma.service.ts
+++ backend/src/modules/auth/auth-prisma.service.ts
@@ -2,14 +2,23 @@ import { AuthService } from './auth.service';
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { LoginCredentialsDto } from './dto/login-credentials.dto';
+import { CustomersService } from '../customers/customers.service';
 
 @Injectable()
 export class AuthPrismaService implements AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly customersService: CustomersService,
+  ) {}
   async validateLoginCredentials(credentials: LoginCredentialsDto) {
     const user = await this.usersService.findUserByEmailAndPassword(
       credentials.email,
       credentials.password,
     );
+
+    switch (user.role) {
+      case 'CUSTOMER':
+        return this.customersService.findCustomerByUserId(user.id);
+    }
   }
 }
--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AuthPrismaService } from './auth-prisma.service';
 import { UsersModule } from '../users/users.module';
+import { CustomersModule } from '../customers/customers.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, CustomersModule],
   exports: [AuthService],
   controllers: [AuthController],
   providers: [{ provide: AuthService, useClass: AuthPrismaService }],
